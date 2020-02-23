e8422965b791ba2a900307c9a492d8850744ef4b
ye-yu <rafolwen98@gmail.com>
2020-02-23 15:39:42 +0800

    Add isin array and dict util functions

--- js/index.js
+++ js/index.js
@@ -6,3 +6,11 @@ function println() {
     console.log(arguments[i]);
   }
 }
+
+function isinarray(obj, value) {
+  return obj.indexOf(value) > -1;
+}
+
+function isinkey(obj, key) {
+  return isinarray(Object.keys(obj), key);
+}
