db3048e928ddb90bd4176fff207f87c5b1b32600
ye-yu <rafolwen98@gmail.com>
2020-02-25 20:17:35 +0800

    Remove println util

--- js/index.js
+++ js/index.js
@@ -1,12 +1,6 @@
 "use strict";
 
 /* utils */
-function println() {
-  for (let i = 0; i < arguments.length; i++) {
-    console.log(arguments[i]);
-  }
-}
-
 function isinarray(obj, value) {
   return obj.indexOf(value) > -1;
 }
