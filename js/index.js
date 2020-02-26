e50fc9910b4e0cd534fd177162eeda0409c833bc
ye-yu <rafolwen98@gmail.com>
2020-02-26 16:09:13 +0800

    Add enumerate generator

--- js/index.js
+++ js/index.js
@@ -16,3 +16,18 @@ function tuplize(obj) {
   }
   return tuple;
 }
+
+function* enumerate(obj) {
+  let counter = 0;
+  if(Array.isArray(obj)) {
+    for(let e of obj) {
+      yield {index: counter, value: e};
+      counter++;
+    }
+  } else {
+    for(let e in obj) {
+      yield {index: counter, value: e};
+      counter++;
+    }
+  }
+}
